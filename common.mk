06dc4e7025ae6770adc118ec188eb73cca94488c
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-09-15 11:56:05 +0300

    sm8150-common: Build missing omx lib
    Signed-off-by: Justin Crawford <Justin@stacksmash.net>

--- common.mk
+++ common.mk
@@ -379,6 +379,7 @@ PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libcodec2_hidl@1.0.vendor \
     libcodec2_vndk.vendor \
+    libmm-omxcore \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -388,6 +389,7 @@ PRODUCT_PACKAGES += \
     libOmxVdec \
     libOmxVenc \
     libOmxVidcCommon \
+    libstagefright_softomx \
     libstagefrighthw
 
 # Power
